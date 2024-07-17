2332842f43de0785dc2a832875c4fbd4e759dfa3
JeremyR <metekohy233@gmail.com>
2024-07-17 14:37:43 +0700

    Initial Commit

---
+++ SpaceFlightNews/SpaceFlightNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFlightNewsApp.swift
+//  SpaceFlightNews
+//
+//  Created by Jeremy Raymond on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFlightNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
