76b5500b935ed04c1873db08b670129e3ba8fe5a
JeremyR <metekohy233@gmail.com>
2024-07-20 13:15:59 +0700

    Initial Commit

---
+++ NewsFlightNewsUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFlightNewsUIKit
+//
+//  Created by Jeremy Raymond on 20/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
