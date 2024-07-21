bf8c4768941149777625dc94ea58836927d2b3b6
JeremyR <metekohy233@gmail.com>
2024-07-21 22:23:53 +0700

    Initial Commit

---
+++ SpaceFlightNewsWithRealm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceFlightNewsWithRealm
+//
+//  Created by Jeremy Raymond on 21/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceFlightNewsWithRealm/SpaceFlightNewsWithRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFlightNewsWithRealmApp.swift
+//  SpaceFlightNewsWithRealm
+//
+//  Created by Jeremy Raymond on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFlightNewsWithRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
