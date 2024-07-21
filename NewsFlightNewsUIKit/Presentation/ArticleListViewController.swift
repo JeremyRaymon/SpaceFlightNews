cb5567379a85848b1e586c50c0e56dd7d89a3d6f
JeremyR <metekohy233@gmail.com>
2024-07-21 16:44:41 +0700

    minor fix

--- NewsFlightNewsUIKit/Presentation/ArticleListViewController.swift
+++ NewsFlightNewsUIKit/Presentation/ArticleListViewController.swift
@@ -21,10 +21,6 @@ class ArticleListViewController: UIViewController {
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Article>!
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSearchBar()
